e6cc02689a8558e033c9ebce7adc6f07cab2eb6e
sabaguchi <sabaguchi.sababa@gmail.com>
2020-12-30 14:14:33 +0900

    fixed_letter_deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'chatーspace'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:sabaguchi/chat-space.git'
