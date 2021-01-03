cd9eacb838709c1101e66a759a8ceb3ec4c7a673
sabaguchi <sabaguchi.sababa@gmail.com>
2021-01-03 17:26:15 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'sabaguchisbucket'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sabaguchisbucket'
+  config.fog_directory  = 'sabaguchibucket'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sabaguchibucket'
 end
